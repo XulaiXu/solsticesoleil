12adf5f21fcfb0683b5efec7392f88d1393901bc
Octavian <xulai_xu@icloud.com>
2025-03-10 15:22:14 -0700

    Fix: Update view imports to use relative paths in routes.js

--- src/routes.js
+++ src/routes.js
@@ -16,14 +16,14 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import Dashboard from "views/Dashboard.js";
-import Notifications from "views/Notifications.js";
-import Icons from "views/Icons.js";
-import Typography from "views/Typography.js";
-import TableList from "views/Tables.js";
-import Maps from "views/Map.js";
-import UserPage from "views/User.js";
-import UpgradeToPro from "views/Upgrade.js";
+import Dashboard from "./views/Dashboard.js";
+import Notifications from "./views/Notifications.js";
+import Icons from "./views/Icons.js";
+import Typography from "./views/Typography.js";
+import TableList from "./views/Tables.js";
+import Maps from "./views/Map.js";
+import UserPage from "./views/User.js";
+import UpgradeToPro from "./views/Upgrade.js";
 
 var routes = [
   {
