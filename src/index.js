ea430457e1bcbca56c867588fd2cf5cd70f54f04
Octavian <xulai_xu@icloud.com>
2025-03-10 14:31:34 -0700

    Fix: Update SCSS and CSS import paths to use relative paths

--- src/index.js
+++ src/index.js
@@ -21,8 +21,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
-import "assets/scss/paper-dashboard.scss?v=1.3.0";
-import "assets/demo/demo.css";
+import "./assets/scss/paper-dashboard.scss?v=1.3.0";
+import "./assets/demo/demo.css";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
 
 import AdminLayout from "./layouts/Admin.js";
