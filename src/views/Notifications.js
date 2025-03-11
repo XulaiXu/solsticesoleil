bda0ec446d1ad4e27844ebf6f1eadfad95b122f6
Octavian <xulai_xu@icloud.com>
2025-03-10 18:01:26 -0700

    feat: clear notifications page content

--- src/views/Notifications.js
+++ src/views/Notifications.js
@@ -16,16 +16,8 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-/*eslint-disable*/
 import React from "react";
-// react plugin for creating notifications
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-// reactstrap components
 import {
-  UncontrolledAlert,
-  Alert,
-  Button,
   Card,
   CardHeader,
   CardBody,
@@ -35,25 +27,6 @@ import {
 } from "reactstrap";
 
 function Notifications() {
-  const notify = (type, message) => {
-    switch (type) {
-      case "info":
-        toast.info(message);
-        break;
-      case "success":
-        toast.success(message);
-        break;
-      case "warning":
-        toast.warning(message);
-        break;
-      case "danger":
-        toast.error(message);
-        break;
-      default:
-        toast(message);
-    }
-  };
-
   return (
     <>
       <div className="content">
@@ -62,188 +35,12 @@ function Notifications() {
             <Card>
               <CardHeader>
                 <CardTitle tag="h5">Notifications</CardTitle>
-                <p className="card-category">
-                  Handcrafted by our friend{" "}
-                  <a target="_blank" href="https://github.com/fkhadra">
-                    Fadi Khadra
-                  </a>
-                  . Please checkout the{" "}
-                  <a href="https://github.com/fkhadra/react-toastify" target="_blank">
-                    full documentation.
-                  </a>
-                </p>
               </CardHeader>
               <CardBody>
-                <Row>
-                  <Col md="6">
-                    <Card className="card-plain">
-                      <CardHeader>
-                        <CardTitle tag="h5">Notifications Style</CardTitle>
-                      </CardHeader>
-                      <CardBody>
-                        <Button
-                          color="info"
-                          block
-                          onClick={() => notify("info", "Welcome to Paper Dashboard React - a beautiful React Bootstrap admin panel")}
-                        >
-                          Info
-                        </Button>
-                        <Button
-                          color="success"
-                          block
-                          onClick={() => notify("success", "Welcome to Paper Dashboard React - a beautiful React Bootstrap admin panel")}
-                        >
-                          Success
-                        </Button>
-                        <Button
-                          color="warning"
-                          block
-                          onClick={() => notify("warning", "Welcome to Paper Dashboard React - a beautiful React Bootstrap admin panel")}
-                        >
-                          Warning
-                        </Button>
-                        <Button
-                          color="danger"
-                          block
-                          onClick={() => notify("danger", "Welcome to Paper Dashboard React - a beautiful React Bootstrap admin panel")}
-                        >
-                          Danger
-                        </Button>
-                      </CardBody>
-                    </Card>
-                  </Col>
-                  <Col md="6">
-                    <Card className="card-plain">
-                      <CardHeader>
-                        <CardTitle tag="h5">Notification states</CardTitle>
-                      </CardHeader>
-                      <CardBody>
-                        <UncontrolledAlert color="primary" fade={false}>
-                          <span>
-                            <b>Primary - </b>
-                            This is a regular notification made with
-                            color="primary"
-                          </span>
-                        </UncontrolledAlert>
-                        <UncontrolledAlert color="info" fade={false}>
-                          <span>
-                            <b>Info - </b>
-                            This is a regular notification made with
-                            color="info"
-                          </span>
-                        </UncontrolledAlert>
-                        <UncontrolledAlert color="success" fade={false}>
-                          <span>
-                            <b>Success - </b>
-                            This is a regular notification made with
-                            color="success"
-                          </span>
-                        </UncontrolledAlert>
-                        <UncontrolledAlert color="warning" fade={false}>
-                          <span>
-                            <b>Warning - </b>
-                            This is a regular notification made with
-                            color="warning"
-                          </span>
-                        </UncontrolledAlert>
-                        <UncontrolledAlert color="danger" fade={false}>
-                          <span>
-                            <b>Danger - </b>
-                            This is a regular notification made with
-                            color="danger"
-                          </span>
-                        </UncontrolledAlert>
-                      </CardBody>
-                    </Card>
-                  </Col>
-                </Row>
               </CardBody>
             </Card>
           </Col>
         </Row>
-        <Row>
-          <Col md="12">
-            <Card>
-              <CardBody>
-                <div className="places-buttons">
-                  <Row>
-                    <Col className="ml-auto mr-auto text-center" md="6">
-                      <CardTitle tag="h4">Notifications Places</CardTitle>
-                      <p className="category">Click to view notifications</p>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="ml-auto mr-auto" lg="8">
-                      <Row>
-                        <Col md="4">
-                          <Button
-                            block
-                            color="primary"
-                            onClick={() => notify("info", "Top Left")}
-                          >
-                            Top Left
-                          </Button>
-                        </Col>
-                        <Col md="4">
-                          <Button
-                            block
-                            color="primary"
-                            onClick={() => notify("info", "Top Center")}
-                          >
-                            Top Center
-                          </Button>
-                        </Col>
-                        <Col md="4">
-                          <Button
-                            block
-                            color="primary"
-                            onClick={() => notify("info", "Top Right")}
-                          >
-                            Top Right
-                          </Button>
-                        </Col>
-                      </Row>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="ml-auto mr-auto" lg="8">
-                      <Row>
-                        <Col md="4">
-                          <Button
-                            block
-                            color="primary"
-                            onClick={() => notify("info", "Bottom Left")}
-                          >
-                            Bottom Left
-                          </Button>
-                        </Col>
-                        <Col md="4">
-                          <Button
-                            block
-                            color="primary"
-                            onClick={() => notify("info", "Bottom Center")}
-                          >
-                            Bottom Center
-                          </Button>
-                        </Col>
-                        <Col md="4">
-                          <Button
-                            block
-                            color="primary"
-                            onClick={() => notify("info", "Bottom Right")}
-                          >
-                            Bottom Right
-                          </Button>
-                        </Col>
-                      </Row>
-                    </Col>
-                  </Row>
-                </div>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-        <ToastContainer />
       </div>
     </>
   );
